5d8935d6fecb369131ee6597076b4ec2ee5f68bb
Panumas20 <74150623+Panumas20@users.noreply.github.com>
2022-01-24 00:43:03 +0700

    final update

---
+++ src/Component/ImageData.js
@@ -0,0 +1,16 @@
+const ImageData = [
+	{
+		title: 'น้ำแข็ง',
+		image: 'https://cdn.pixabay.com/photo/2021/04/28/23/58/ice-6215207_960_720.jpg',
+	},
+	{
+		title: 'ภูเขา',
+		image: 'https://cdn.pixabay.com/photo/2021/12/13/19/10/nature-6868923_960_720.jpg',
+	},
+	{
+		title: 'ผึ้ง',
+		image: 'https://cdn.pixabay.com/photo/2021/09/13/06/39/bee-6620435_960_720.jpg',
+	},
+];
+
+export default ImageData;
